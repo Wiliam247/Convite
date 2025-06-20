3b94f88c8c2fef0e450e4d4f27e926c2fe98fcfd
Wiliam247 <subzeroyt5564@gmail.com>
2025-06-20 17:15:50 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const envelope = document.getElementById('envelope');
-    const inviteWrapper = document.getElementById('invite-wrapper');
-    const container = document.querySelector('.container');
-
-    envelope.addEventListener('click', () => {
-        container.classList.add('invite-active');
-    });
-});
